ca96337fc8e9317361e40a9a044b5b9e84f1a868
Eimm7 <m0137038101@gmail.com>
2025-05-29 15:19:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Set the app title 
-st.title('Convert Your Money App !!') 
+st.title('Convert My No Money App....UUUUWAAAAA !!') 
 
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
